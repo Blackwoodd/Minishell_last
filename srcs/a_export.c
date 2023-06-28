c31287c0d06052c5363708a3bb7e4792940ee87a
Nathan <nathan.bechon@free.fr>
2023-06-28 23:24:46 +0200

    myexport

--- srcs/a_export.c
+++ srcs/a_export.c
@@ -12,21 +12,47 @@
 
 #include "../include/minishell.h"
 
-int	commande_export(char **tab)
+int     commande_export(char **tab)
 {
-	int		i;
-	t_env	*envar;
+        int             i;
+        t_env   *envar;
 
-	i = 0;
-	envar = get_envar();
-	if (envar == NULL || envar->env_var == NULL)
-		return (EXIT_FAILURE);
-	if (tab[1] != NULL && tab[1][0] == '-')
-		return (EXIT_FAILURE);
-	while (envar->env_var && envar->env_var[i] != NULL)
-	{
-		if (ft_printf("export %s\n", envar->env_var[i++]) == 0)
-			return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
+        i = 0;
+        envar = get_envar();
+        if (envar == NULL || envar->env_var == NULL)
+                return (EXIT_FAILURE);
+        if (tab[1] != NULL && tab[1][0] == '-')
+                return (EXIT_FAILURE);
+        if (tab[1] == NULL)
+        {
+                while (envar->env_var && envar->env_var[i] != NULL)
+                {
+                        if (ft_printf("export %s\n", envar->env_var[i++]) == 0)
+                                return (EXIT_FAILURE);
+                }
+        }
+        else
+        {
+                int num_env_vars = 0;
+                while (envar->env_var[num_env_vars] != NULL)
+                        num_env_vars++;
+                char *new_env_var = malloc(strlen(tab[1]) + 1);
+                if (new_env_var == NULL)
+                {
+                        printf("Erreur lors de l'allocation de mémoire.\n");
+                        return (EXIT_FAILURE);
+                }
+                strcpy(new_env_var, tab[1]);
+                char **new_env_var_list = realloc(envar->env_var, (num_env_vars + 2) * sizeof(char *));
+                if (new_env_var_list == NULL)
+                {
+                        printf("Erreur lors de l'allocation de mémoire.\n");
+                        free(new_env_var);
+                        return (EXIT_FAILURE);
+                }
+                new_env_var_list[num_env_vars] = new_env_var;
+                new_env_var_list[num_env_vars + 1] = NULL;
+                envar->env_var = new_env_var_list;
+        }
+        return (EXIT_SUCCESS);
 }
