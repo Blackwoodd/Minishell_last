54fb67365a6486d244bc26520271e10c5eaa5691
Nathan <nathan.bechon@free.fr>
2023-07-07 15:35:40 +0200

    perfectadd

--- srcs/a_export.c
+++ srcs/a_export.c
@@ -12,112 +12,156 @@
 
 #include "../include/minishell.h"
 
-int     commande_export(char **tab)
+int     verif_option(char **tab)
 {
-	int             count;
-        int             i;
-	int		j;
-        t_env   	*envar;
-
-        i = 0;
-        count = 0;
-	j = 1;
-	while (tab[j])
-        	j++;
-        j--;
-        envar = get_envar();
-	if (tab[1] != NULL && (tab[1][0] < '!' || tab[1][0] > '}'))
-		tab[1] = NULL;
-        if (envar == NULL || envar->env_var == NULL)
-                return (EXIT_FAILURE);
         if (tab[1] != NULL)
         {
                 if (tab[1][0] == '-')
                 {
                         printf("export: -%c: invalid option\n", tab[1][1]);
-                        return (EXIT_FAILURE);
+                        return (1);
                 }
-                while (tab[1][i])
+        }
+        return (0);
+}
+
+int     verif_event(char **tab, int count)
+{
+        int     i;
+        int     j;
+
+        j = 1;
+        while (j <= count)
+        {
+                i = 0;
+                while (tab[j][i])
                 {
-                        if (tab[1][i] == '=')
-				count++;
-                        if (tab[1][i] == '!')
+                        if (tab[j][i] == '!')
                         {
-                                while (tab[1][i])
+                                while (tab[j][i])
                                 {
-                                        printf ("%c", tab[1][i]);
+                                        printf ("%c", tab[j][i]);
                                         i++;
                                 }
-				printf (": event not found\n");
-				return (EXIT_FAILURE);
+                                printf (": event not found\n");
+                                return (1);
                         }
                         i++;
                 }
+                j++;
+        }
+        return (0);
+}
+
+int     verif_export_valid(char **tab, int count)
+{
+        int     i;
+        int     j;
+
+        j = 1;
+        while (j <= count)
+        {
+                i = 0;
+                if (tab[j] == NULL)
+                        j++;
+                else if (tab[j][0] == '=' || (tab[j][0] >= '0' && tab[j][0] <= '9'))
+                {
+                          printf ("export: `%s': not a valid identifier\n", tab[j]);
+                          return (1);
+                }
+                while (tab[j][i] != '=')
+                {
+                        if (tab[j][i] == '\0')
+                                break ;
+                        else if ((tab[j][i] >= 'A' && tab[j][i] <= 'Z') || (tab[j][i] >= 'a' && tab[j][i] <= 'z')  || (tab[j][i] >= '0' && tab[j][i] <= '9'))
+                                i++;
+                        else
+                        {
+                                printf ("export: `%s': not a valid identifier\n", tab[j]);
+                                return (1);
+                        }
+                }
+                j++;
+        }
+        return (0);
+}
+
+int     commande_export(char **tab)
+{
+        int             count;
+        int             i;
+        int             j;
+        t_env           *envar;
+
+        count = 0;
+        j = 1;
+        i = 0;
+        while (tab[j])
+                j++;
+        j--;
+        envar = get_envar();
+        if (tab[1] != NULL && (tab[1][0] < '!' || tab[1][0] > '}'))
+                tab[1] = NULL;
+        if (envar == NULL || envar->env_var == NULL)
+                return (EXIT_FAILURE);
+        if (verif_option(tab) == 1)
+                return (EXIT_FAILURE);
+        if (tab[1] != NULL)
+        {
+                while (tab[1][i])
+                {
+                        if (tab[1][i] == '=')
+                                count++;
+                       i++;
+                }
+                if (verif_event(tab, j) == 1)
+                        return (EXIT_FAILURE);
         }
         if (tab[1] != NULL && tab[2] != NULL && count < 1)
         {
                 printf ("export: %s: not a valid identifier\n", tab[2]);
                 return (EXIT_FAILURE);
         }
-        i = 0;
-        if (tab[1] != NULL)
+        if (verif_export_valid(tab, j) == 1)
+                return (EXIT_FAILURE);
+        if (j == 0)
         {
-                if (tab[1][0] == '=' || (tab[1][0] >= '0' && tab[1][0] <= '9'))
+                while (envar->env_var && envar->env_var[i] != NULL)
                 {
-                          printf ("export: `%s': not a valid identifier\n", tab[1]);
-                          return (EXIT_FAILURE);
-                }
-                while (tab[1][i] != '=')
-                {
-			if (tab[1][i] == '\0')
-				break ;
-                        else if ((tab[1][i] >= 'A' && tab[1][i] <= 'Z') || (tab[1][i] >= 'a' && tab[1][i] <= 'z')  || (tab[1][i] >= '0' && tab[1][i] <= '9'))
-                                i++;
-                        else
-                        {
-                                printf ("export: `%s': not a valid identifier\n", tab[1]);
+                        if (ft_printf("export %s\n", envar->env_var[i++]) == 0)
                                 return (EXIT_FAILURE);
-                        }
                 }
         }
-        i = 0;
-	if (j == 0)
-	{
-		while (envar->env_var && envar->env_var[i] != NULL)
-		{
-			if (ft_printf("export %s\n", envar->env_var[i++]) == 0)
-				return (EXIT_FAILURE);
-		}
-	}
-	i = 1;
+        i = 1;
         while (i <= j)
-	{
-		if (tab[i] == NULL)
-			printf ("export: `': not a valid identifier\n");
-		else
-		{
-			int num_env_vars = 0;
-               		while (envar->env_var[num_env_vars] != NULL)
-                       		num_env_vars++;
-                	char *new_env_var = malloc(strlen(tab[i]) + 1);
-                	if (new_env_var == NULL)
-                	{
-                       		printf("Erreur lors de l'allocation de mémoire.\n");
-                       		return (EXIT_FAILURE);
-                	}
-                	strcpy(new_env_var, tab[i]);
-                	char **new_env_var_list = realloc(envar->env_var, (num_env_vars + 2) * sizeof(char *));
-                	if (new_env_var_list == NULL)
-                	{
-                       		printf("Erreur lors de l'allocation de mémoire.\n");
-                       		free(new_env_var);
-				return (EXIT_FAILURE);
-                	}
-                	new_env_var_list[num_env_vars] = new_env_var;
-                	new_env_var_list[num_env_vars + 1] = NULL;
-                	envar->env_var = new_env_var_list;
-		}
-		i++;
-	}
+        {
+                if (tab[i] == NULL)
+                        printf ("export: `': not a valid identifier\n");
+                else
+                {
+                        int num_env_vars = 0;
+                        while (envar->env_var[num_env_vars] != NULL)
+                                num_env_vars++;
+                        char *new_env_var = malloc(strlen(tab[i]) + 1);
+                        if (new_env_var == NULL)
+                        {
+                                printf("Erreur lors de l'allocation de mémoire.\n");
+                                return (EXIT_FAILURE);
+                        }
+                        strcpy(new_env_var, tab[i]);
+                        char **new_env_var_list = realloc(envar->env_var, (num_env_vars + 2) * sizeof(char *));
+                        if (new_env_var_list == NULL)
+                        {
+                                printf("Erreur lors de l'allocation de mémoire.\n");
+                                free(new_env_var);
+                                return (EXIT_FAILURE);
+                        }
+                        new_env_var_list[num_env_vars] = new_env_var;
+                        new_env_var_list[num_env_vars + 1] = NULL;
+                        envar->env_var = new_env_var_list;
+                }
+                i++;
+        }
         return (EXIT_SUCCESS);
 }
+
--- srcs/a_unset.c
+++ srcs/a_unset.c
@@ -12,117 +12,156 @@
 
 #include "../include/minishell.h"
 
-static char	*ft_get_var(t_env *envar, int i)
-{
-	char	**split;
-	char	*var;
-
-	split = ft_split(envar->env_var[i], '=');
-	if (split == NULL)
-		return (NULL);
-	var = ft_strdup(split[0]);
-	ft_free_str_array(&split);
-	return (var);
+static char     *ft_get_var(t_env *envar, int i)
+{       
+        char    **split;
+        char    *var;
+        
+        split = ft_split(envar->env_var[i], '=');
+        if (split == NULL)
+                return (NULL);
+        var = ft_strdup(split[0]);
+        ft_free_str_array(&split);
+        return (var);
 }
 
-static void	ft_suppr_var(t_env *envar, int i)
-{
-	int	j;
-
-	ft_free_str(&envar->env_var[i]);
-	j = i;
-	while (envar->env_var[j + 1])
-	{
-		envar->env_var[j] = envar->env_var[j + 1];
-		j++;
-		if (envar->env_var[j + 1] == NULL)
-			envar->env_var[j] = NULL;
-		i = ft_str_arr_len(envar->env_var);
-	}
+static void     ft_suppr_var(t_env *envar, int i)
+{       
+        int     j;
+        
+        ft_free_str(&envar->env_var[i]);
+        j = i;
+        while (envar->env_var[j + 1])
+        {       
+                envar->env_var[j] = envar->env_var[j + 1];
+                j++;
+                if (envar->env_var[j + 1] == NULL)
+                        envar->env_var[j] = NULL;
+                i = ft_str_arr_len(envar->env_var);
+        }
 }
 
-static int	norm_unset(char **tab, t_env *envar, int i, int j)
-{
-	char	*var;
-
-	var = NULL;
-	if (ft_strcmp(envar->env_var[i], tab[j]) == 0
-		|| ft_strcmp(envar->env_var[i], tab[j]) == 61)
-	{
-		var = ft_get_var(envar, i);
-		if (var == NULL)
-			return (EXIT_FAILURE);
-		if (ft_strncmp(var, tab[j], ft_strlen(var)) != 0
-			|| ft_strncmp(var, tab[j], ft_strlen(tab[j]) != 0))
-		{
-			ft_free_str(&var);
-			return (2);
-		}
-		ft_suppr_var(envar, i);
-	}
-	ft_free_str(&var);
-	return (EXIT_SUCCESS);
+static int      norm_unset(char **tab, t_env *envar, int i, int j)
+{       
+        char    *var;
+        
+        var = NULL;
+        if (ft_strcmp(envar->env_var[i], tab[j]) == 0
+                || ft_strcmp(envar->env_var[i], tab[j]) == 61)
+        {       
+                var = ft_get_var(envar, i);
+                if (var == NULL)
+                        return (EXIT_FAILURE);
+                if (ft_strncmp(var, tab[j], ft_strlen(var)) != 0
+                        || ft_strncmp(var, tab[j], ft_strlen(tab[j]) != 0))
+                {       
+                        ft_free_str(&var);
+                        return (2);
+                }
+                ft_suppr_var(envar, i);
+        }
+        ft_free_str(&var);
+        return (EXIT_SUCCESS);
 }
 
-int	commande_unset(char **tab)
+int     verif_unset(char *tab)
+{
+        int     i;
+        int     j;
+
+        j = 0;
+        if (tab[j] == '-')
+        {
+                printf ("unset: -%c: invalid option\n", tab[j + 1]);
+                return (1);
+        }
+        while (tab[j])
+        {
+                if (tab[j] == '_' || (tab[j] >= 'A' && tab[j] <= 'Z') || (tab[j] >= 'a' && tab[j] <= 'z')  || (tab[j] >= '0' && tab[j] <= '9'))
+                        j++;
+                else if (tab[j] == '!')
+                {
+                        i = j;
+                        while (tab[i])
+                        {
+                                printf ("%c", tab[i]);
+                                i++;
+                        }
+                        printf (": event not found\n");
+                        return (1);
+                }
+                else
+                {
+                        printf ("export: `%s': not a valid identifier\n", tab);
+                        return (0);
+                        j++;
+                }
+        }
+        return (0);
+}
+
+int     commande_unset(char **tab)
 {
         int             i;
         int             j;
-	int		count;
-	int		for_exit;
+        int             count;
+        int             for_exit;
         int             out;
         t_env   *envar;
 
         envar = get_envar();
-	if (tab[1] == NULL)
+        if (tab[1] == NULL)
                 return (EXIT_SUCCESS);
         j = 0;
+        i = 0;
         if (tab[1][0] >= '0' && tab[1][0] <= '9')
         {
                 printf ("export: `%s': not a valid identifier\n", tab[1]);
                 return (EXIT_FAILURE);
         }
-	count = 1;
-	for_exit = 0;
-	while (tab[count] != NULL)
-	{
-		j = 0;
-		if (tab[count][j] == '-')
-		{
-			printf ("unset: -%c: invalid option\n", tab[count][j + 1]);
-			return (EXIT_FAILURE);
-		}
-        	while (tab[count][j])
-        	{
-                	if (tab[count][j] == '_' || (tab[count][j] >= 'A' && tab[count][j] <= 'Z') || (tab[count][j] >= 'a' && tab[count][j] <= 'z')  || (tab[count][j] >= '0' && tab[count][j] <= '9'))
-                        	j++;
-                	else if (tab[count][j] == '!')
-                	{
-                        	i = j;
-                        	while (tab[count][i])
-                        	{
-                                	printf ("%c", tab[count][i]);
-                               	 	i++;
-                        	}
-                        	printf (": event not found\n");
-                        	return (EXIT_FAILURE);
-                	}
-                	else
-                	{
-                        	printf ("export: `%s': not a valid identifier\n", tab[count]);
-                        	for_exit = 1;
-				j++;
-                	}
-        	}
-		count++;
-	}
-	if (for_exit == 1)
-		return (EXIT_FAILURE);
-	if (tab[1] != NULL && (tab[1][0] <= '!' || tab[1][0] >= '}'))
-	{
-		printf ("unset: `': not a valid identifier\n");
-		return (EXIT_FAILURE);
-	}
+        count = 1;
+        for_exit = 0;
+        while (tab[count] != NULL)
+        {
+                if (verif_unset(tab[count]) == 1)
+                        return (EXIT_FAILURE);
+                //j = 0;
+                //if (tab[count][j] == '-')
+                //{     
+                //      printf ("unset: -%c: invalid option\n", tab[count][j + 1]);
+                //              return (EXIT_FAILURE);
+                //}
+                //while (tab[count][j])
+                //{     
+                //      if (tab[count][j] == '_' || (tab[count][j] >= 'A' && tab[count][j] <= 'Z') || (tab[count][j] >= 'a' && tab[count][j] <= 'z')  || (tab[count][j] >= '0' && tab[count][j] <= '9'))
+                //              j++;
+                //      else if (tab[count][j] == '!')
+                //      {       
+                //              i = j;
+                //              while (tab[count][i])
+                //              {       
+                //                      printf ("%c", tab[count][i]);
+                //                              i++;
+                //              }
+                //              printf (": event not found\n");
+                //              return (EXIT_FAILURE);
+                //      }
+                //      else
+                //      {       
+                //              printf ("export: `%s': not a valid identifier\n", tab[count]);
+                //              for_exit = 1;
+                //              j++;
+                //      }
+                //}
+                count++;
+        }
+        if (for_exit == 1)
+                return (EXIT_FAILURE);
+        if (tab[1] != NULL && (tab[1][0] <= '!' || tab[1][0] >= '}'))
+        {
+                printf ("unset: `': not a valid identifier\n");
+                return (EXIT_FAILURE);
+        }
         j = 1;
         while (tab[j])
         {
@@ -140,4 +179,3 @@ int	commande_unset(char **tab)
         }
         return (EXIT_SUCCESS);
 }
-
